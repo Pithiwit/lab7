9e76f1bf93e9f6c276e4d2b638469966900245a8
Pithiwit <wazadafa14@gmail.com>
2023-12-12 14:21:06 +0700

    lab7_2 completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,18 +1,30 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
+string name,movie,date;
+int id,stid,x;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+int main(){
+cout << "Fahsai: " << "Sawadee ka...Can you tell me your name?"<<endl;
+getline(cin,name);
+cout << "?????: ";
+cout << "Fahsai: " << "Wow!!! " << name << " is a really cool name."<<endl;
+cout << "Fahsai: " << "I think you are an Engineering student. What is your student ID?"<<endl;
+cin >> id;
+x = (id/10000000)-12;
+stid=x;
+cout << name << ": ";
+cout << "Fahsai: " << "I think you may be GEAR "<< stid << ". I have a free movie ticket for you."<<endl;
+cout << "Fahsai: " <<"Let's go to the cinema together!!!"<<endl;
+cout << "Fahsai: " <<"What movie do you want to watch?"<<endl;
+cin.ignore();
+getline(cin,movie);
+cout << name << ": ";
+cout << "Fahsai: "<< "So....which day are you free to go with me?"<<endl;
+getline(cin,date);
+cout << name << ": ";
+cout << "Fahsai: " << date << "....that is OK!!! I'm looking forward to watching " << movie <<" with you."<<endl;
+cout << name <<": ";
+cout << "Fahsai: " <<"555+ see you " <<date << ". Bye Bye \\(^ ^)/"<<endl;
+return 0;
+}
