f61f743c85cb267dc520b044a71ca0c7d730f978
Pithiwit <wazadafa14@gmail.com>
2023-12-12 14:30:51 +0700

    lab7_3 completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -6,14 +6,18 @@ char before(char x){
 }
 
 int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+//ไม่ต้องเติม function main()
+
+char before(char x){
+if(x<='Z'&&x>'A'){
+return x-1;
+}
+if(x=='A'){
+return 'Z';
+}
+if(x=='0'){
+return'0';
+}
+return '0';
+}
 }
